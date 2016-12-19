b7b5021150bca320a7721b1aac209be3482c2b1c
An Doan <minh.an.doan@gmail.com>
2016-12-18 22:31:07 -0800

    add lab 1

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                Console.WriteLine("Hello " + String.Join(", ", args));
+            }
+            else
+            {
+                Console.WriteLine("Please enter an argument after lab1");
+            }
+        }
+    }
+}
