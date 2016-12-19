b4765c670846658d05542bb76d4233e59f6d3501
An Doan <minh.an.doan@gmail.com>
2016-12-18 22:34:53 -0800

    add lab 4

---
+++ Lab4-Methods/Windows/Lab4/Lab4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    /// <summary>
+    /// Implements Lab 4 functionality
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Implements Lab 4 functionality
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // create a new deck and print the contents of the deck
+            Deck deck = new Deck();
+            Console.WriteLine("========================================================");
+            Console.WriteLine("=============Deck before shuffle...======================");
+            Console.WriteLine("========================================================");
+            deck.Print();
+
+            // shuffle the deck and print the contents of the deck
+            deck.Shuffle();
+            Console.WriteLine("========================================================");
+            Console.WriteLine("=============Deck after shuffle...======================");
+            Console.WriteLine("========================================================");
+            deck.Print();
+
+            Console.WriteLine();
+
+            // take the top card from the deck and print the card rank and suit
+            Card topCard = deck.TakeTopCard();
+            Console.WriteLine("The first top card is a " + topCard);
+            Console.WriteLine();
+
+            // take the top card from the deck and print the card rank and suit
+            topCard = deck.TakeTopCard();
+            Console.WriteLine("The second top card is a " + topCard);
+            Console.WriteLine();
+
+        }
+    }
+}
